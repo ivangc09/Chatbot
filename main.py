37d38554c19d9209c71920e2b8f1de41388a2e4f
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-07-31 15:29:23 -0600

    Chatbot con modelo multietiqueta

--- main.py
+++ main.py
@@ -1,102 +1,37 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch, json
+import torch
+import torch.nn.functional as F
+import json
 import gdown
-import zipfile
 import os
+import zipfile
 
-url = "https://drive.google.com/uc?id=1LFEOEliakObh1GAhTFPGuEHR7vYPwrTE"
-output = "modelo_chatbot.zip"
+#url = "https://drive.google.com/uc?id=1LFEOEliakObh1GAhTFPGuEHR7vYPwrTE"
+#output = "modelo_chatbot.zip"
+
+url = "https://drive.google.com/uc?id=1EBgyASxpQG1cVdmtFF-ELsPi_Z8FZdHM"
+output = "Chatbot_Multilabel.zip"
 
 # Descagar el modelo del chatbot si no existe
-if not os.path.exists("modelo_chatbot"):
+if not os.path.exists("Chatbot_Multilabel"):
     gdown.download(url, output, quiet=False)
 
     with zipfile.ZipFile(output, 'r') as zip_ref:
-        zip_ref.extractall("modelo_chatbot")
-
-labels = [
-    "Abyssinian",
-    "Bengal",
-    "Birman",
-    "Bombay",
-    "British_Shorthair",
-    "Egyptian_Mau",
-    "Maine_Coon",
-    "Persian",
-    "Ragdoll",
-    "Russian_Blue",
-    "Siamese",
-    "Sphynx",
-    "american_bulldog",
-    "american_pit_bull_terrier",
-    "american_shorthair",
-    "australian_shepherd",
-    "basset_hound",
-    "beagle",
-    "border_collie",
-    "boxer",
-    "chartreux",
-    "chihuahua",
-    "cornish_rex",
-    "dachshund",
-    "doberman",
-    "english_cocker_spaniel",
-    "english_setter",
-    "french_bulldog",
-    "german_shorthaired",
-    "golden_retriever",
-    "great_pyrenees",
-    "havanese",
-    "himalayan",
-    "japanese_chin",
-    "keeshond",
-    "leonberger",
-    "miniature_pinscher",
-    "newfoundland",
-    "norwegian_forest_cat",
-    "persian",
-    "pitbull",
-    "pomeranian",
-    "poodle",
-    "pug",
-    "rottweiler",
-    "saint_bernard",
-    "samoyed",
-    "scottish_fold",
-    "scottish_terrier",
-    "shiba_inu",
-    "siamese",
-    "siberian_husky",
-    "sintoma_convulsiones",
-    "sintoma_diarrea",
-    "sintoma_falta_apetito",
-    "sintoma_moco_nasal",
-    "sintoma_ojos_rojos",
-    "sintoma_vomito",
-    "sphynx",
-    "staffordshire_bull_terrier",
-    "turkish_angora",
-    "wheaten_terrier",
-    "yorkshire_terrier"
-]
-
-id2label = {str(i): label for i, label in enumerate(labels)}
-label2id = {label: i for i, label in enumerate(labels)}
-
-
-MODEL_PATH = os.path.abspath("modelo_chatbot")
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-model = AutoModelForSequenceClassification.from_pretrained(
-    MODEL_PATH,
-    id2label=id2label,
-    label2id=label2id
-    )
-model.eval()
+        zip_ref.extractall("Chatbot_Multilabel")
 
 with open("respuestas.json", encoding="utf-8") as f:
     respuestas = json.load(f)
+
+with open("id2label.json", encoding="utf-8") as f:
+    id2label = json.load(f)
+
+
+MODEL_PATH = os.path.abspath("Chatbot_Multilabel")
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
+model.eval()
 
 app = FastAPI()
 
@@ -110,12 +45,37 @@ def responder(pregunta: Pregunta):
         
         with torch.no_grad():
             outputs = model(**inputs)
-            pred_id = int(outputs.logits.argmax(dim=-1))
-            categoria = id2label.get(str(pred_id), "desconocido")
+            probs = F.sigmoid(outputs.logits)[0]
         
+        etiquetas = [
+            id2label[str(i)] for i,p in enumerate(probs) if probs[i] > 0.5
+        ]
 
-        respuesta = respuestas.get(categoria, "Lo siento, no tengo información sobre eso.")
-        return {"categoria": categoria, "respuesta": respuesta}
+        print("Etiquetas predichas:", etiquetas)
+
+        
+        raza = None
+        temas = []
+
+        for etiqueta in etiquetas:
+            if etiqueta in respuestas:
+                raza = etiqueta
+            elif etiqueta in ["cuidados","enfermedad","info_general"]:
+                temas.append(etiqueta)
+
+        if not raza:
+            return {"respuestas": [{"categoria": "desconocido", "respuesta": "No pude identificar la raza mencionada."}]}
+        
+        if not temas:
+            temas = ["info_general"]
+
+        resultados = []
+
+        for tema in temas:
+            respuesta = respuestas.get(raza, {}).get(tema, f"No tengo información sobre {tema} para {raza}")
+            resultados.append({"categoria": f"{raza}_{tema}", "respuesta": respuesta})
+
+        return {"respuestas": resultados}
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
